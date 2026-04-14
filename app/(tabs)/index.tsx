d14e240e9f1793c1e7036fd37e5295d68da1317e
sayak53 <sayakchakraborty928@gmail.com>
2026-04-14 18:59:31 +0530

    handletask function added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,14 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
   const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const handleAddTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
 
   return (
     <View style={{ flex: 1, padding: 20, backgroundColor: "white" }}>
@@ -26,15 +34,24 @@ export default function HomeScreen() {
       />
 
       <TouchableOpacity
+        onPress={handleAddTask}
         style={{
           backgroundColor: "#3498db",
           padding: 12,
           borderRadius: 6,
           alignItems: "center",
+          marginBottom: 20,
         }}
       >
         <Text style={{ color: "white", fontSize: 16 }}>Add</Text>
       </TouchableOpacity>
+
+      {/* Show tasks */}
+      {tasks.map((item, index) => (
+        <Text key={index} style={{ fontSize: 18, marginBottom: 10 }}>
+          • {item}
+        </Text>
+      ))}
     </View>
   );
 }
